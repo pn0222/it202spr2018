d2ed1daf1c7a95ef9cde29b3dd5442fa8788dc9b
Paul Nguyen <pnguye40@uic.edu>
2018-04-01 03:05:46 +0000

    Testing step 5

--- projects/p4/scripts/app.js
+++ projects/p4/scripts/app.js
@@ -50,10 +50,17 @@
     var selected = select.options[select.selectedIndex];
     var key = selected.value;
     var label = selected.textContent;
-    // TODO init the app.selectedCities array here
+    if (!app.selectedCities) {
+      app.selectedCities = [];
+    }
     app.getForecast(key, label);
-    // TODO push the selected city to the array and save here
+    app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
+    // TODO init the app.selectedCities array here
+    // app.getForecast(key, label);
+    // TODO push the selected city to the array and save here
+    // app.toggleAddDialog(false);
   });
 
   document.getElementById('butAddCancel').addEventListener('click', function() {
@@ -197,6 +204,11 @@
   };
 
   // TODO add saveSelectedCities function here
+  
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    localStorage.selectedCities = selectedCities;
+  };
 
   app.getIconClass = function(weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
@@ -304,6 +316,24 @@
   };
 
   // TODO add startup code here
+  app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+  /* The user is using the app for the first time, or the user has not
+  * saved any cities, so show the user some fake data. A real app in this
+  * scenario could guess the user's location via IP lookup and then inject
+  * that data into the page.
+  */
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+      ];
+    app.saveSelectedCities();
+  }
 
   // TODO add service worker code here
 })();
